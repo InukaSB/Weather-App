84a4dd55c7495dfab7b60ceee26784d0ab88dfcb
Inuka Batawala <39651773+InukaSB@users.noreply.github.com>
2020-10-23 19:02:58 +0530

    darkmode according to sun position.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', () => {
    let long;
    let lat;
+   let longShort;
+   let latShort;
    let temperatureDescription = document.querySelector('.temperature-discription');
    let temperatureDegree = document.querySelector('.temperature-degree');
    let locationTimezone = document.querySelector('.location-timezone');
@@ -18,19 +20,33 @@ window.addEventListener('load', () => {
    let darkMode = document.querySelector('.darkMode');
    const currentDate = new Date();
    const hourNow = currentDate.getHours();
+   const minuteNow = currentDate.getMinutes();
+   const secondsNow = currentDate.getSeconds();
+   const timeNow = hourNow + ":" + minuteNow + ":" + secondsNow;
+   const timeNowOneLine = timeNow.replace(/:/g, "");
+
    // var hourNow = getHours();
    // var minuteNow = getMinutes();
    // var timeNow = hourNow + ":" + minuteNow;
 
-   console.log(currentDate);
+   console.log(timeNowOneLine);
+
+
 
    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
          long = position.coords.longitude;
          lat = position.coords.latitude;
+         longShort = long.toFixed(6);
+         latShort = lat.toFixed(6);
+
 
          const proxy = "https://cors-anywhere.herokuapp.com/";
-         const api = `${proxy}https://api.darksky.net/forecast/ffaa6cee76469a3b80ef8e2d5766bdc0/${lat},${long}`;
+         const api = `${proxy}https://api.darksky.net/forecast/ffaa6cee76469a3b80ef8e2d5766bdc0/${latShort},${longShort}`;
+         const sunPositionApi = "https://api.sunrise-sunset.org/json?lat=" + latShort + "&lng=" + longShort;
+         // const sunPositionApi = `https://rapidapi.p.rapidapi.com/api/sun/?latitude=${lat}&longitude=-${long}`
+
+
 
          fetch(api)
             .then(response => {
@@ -48,6 +64,9 @@ window.addEventListener('load', () => {
                locationWindspeed.textContent = Math.floor(windSpeed);
                locationUVIndex.textContent = uvIndex;
 
+               console.log("lat:" + latShort);
+               console.log("long:" + longShort);
+
                //Formula for celcius
 
                let celsius = (temperature - 32) * (5 / 9);
@@ -57,10 +76,11 @@ window.addEventListener('load', () => {
 
                setIcons(icon, document.querySelector('.icon'), "white");
 
+
                // switch color mode
                darkMode.addEventListener('click', () => {
 
-                  if(document.querySelector(".darkMode p").textContent == "Dark Mode"){
+                  if (document.querySelector(".darkMode p").textContent == "Dark Mode") {
                      document.body.style.color = "white";
                      document.body.style.background = "#12505f";
                      document.querySelector(".app").style.background = "rgb(32, 32, 32)";
@@ -76,29 +96,29 @@ window.addEventListener('load', () => {
                      setIcons(icon, document.querySelector('.icon'), "black");                     // document.querySelector(".darkMode").style.background = "rgb(32, 32, 32)";
                      // document.querySelector(".darkMode p").style.color = "white";
                   }
-               
+
                });
-               
-   
+
+
                //change temperature to Celcius/Faranheit
-               temperatureSection.addEventListener('click', ()=>{
-                  if(temperatureSpan.textContent === "°F"){
-                     temperatureSpan.textContent ="°C";
+               temperatureSection.addEventListener('click', () => {
+                  if (temperatureSpan.textContent === "°F") {
+                     temperatureSpan.textContent = "°C";
                      temperatureSpanNonActive.textContent = "| °F";
                      temperatureDegree.textContent = Math.floor(celsius);
                      sound.currentTime = 0;
                      sound.play();
                   } else {
-                     temperatureSpan.textContent ="°F";
+                     temperatureSpan.textContent = "°F";
                      temperatureDegree.textContent = Math.floor(temperature);
                      temperatureSpanNonActive.textContent = "| °C";
                      sound.currentTime = 0;
                      sound.play();
                   }
-               }); 
+               });
 
-               locationWindSection.addEventListener('click', () =>{
-                  if(windSpan.textContent === "mph"){
+               locationWindSection.addEventListener('click', () => {
+                  if (windSpan.textContent === "mph") {
                      windSpan.textContent = "km/h";
                      windSpanNext.textContent = "| mph";
                      locationWindspeed.textContent = Math.floor(kmh);
@@ -116,33 +136,62 @@ window.addEventListener('load', () => {
 
                // color Mode babsed on time
 
-               if(hourNow >= 18){
-                  document.body.style.color = "white";
-                  document.body.style.background = "#12505f";
-                  document.querySelector(".app").style.background = "rgb(32, 32, 32)";
-                  document.querySelector(".darkMode p").textContent = "Light Mode";
-                  setIcons(icon, document.querySelector('.icon'), "white");
-                  // document.querySelector(".darkMode").style.background = "white";
-                  // document.querySelector(".darkMode p").style.color = "black";
-               } else {
-                  document.body.style.color = "black";
-                  document.body.style.background = "#6dd5ed";
-                  document.querySelector(".app").style.background = "white";
-                  document.querySelector(".darkMode p").textContent = "Dark Mode";
-                  setIcons(icon, document.querySelector('.icon'), "black");                     // document.querySelector(".darkMode").style.background = "rgb(32, 32, 32)";
-                  // document.querySelector(".darkMode p").style.color = "white";
-               }
+               // fetch(sunPositionApi, {
+               //    "method": "GET",
+               //    "headers": {
+               //       "x-rapidapi-host": "sun.p.rapidapi.com",
+               //       "x-rapidapi-key": "b1d2e3fa49msh854c1a5ae21195bp18bdb3jsna0d87843d988"
+               //    }
+               // })
+
+               fetch(sunPositionApi)
+                  .then(response => {
+                     return response.json();
+                  })
+                  .then(data => {
+                     console.log(data);
+                     const { sunrise, sunset } = data.results;
+                     const sunriseTime = replaceSunPositionText(sunrise);
+                     const sunsetTime = replaceSunPositionText(sunset);
+
+                     console.log("sunset:" + sunsetTime);
+                     console.log('sunrise:' + sunriseTime);
+
+                     if (timeNowOneLine >= sunsetTime && timeNowOneLine < sunriseTime) {
+                        document.body.style.color = "white";
+                        document.body.style.background = "#12505f";
+                        document.querySelector(".app").style.background = "rgb(32, 32, 32)";
+                        document.querySelector(".darkMode p").textContent = "Light Mode";
+                        setIcons(icon, document.querySelector('.icon'), "white");
+                        // document.querySelector(".darkMode").style.background = "white";
+                        // document.querySelector(".darkMode p").style.color = "black";
+                     } else {
+                        document.body.style.color = "black";
+                        document.body.style.background = "#6dd5ed";
+                        document.querySelector(".app").style.background = "white";
+                        document.querySelector(".darkMode p").textContent = "Dark Mode";
+                        setIcons(icon, document.querySelector('.icon'), "black");                     // document.querySelector(".darkMode").style.background = "rgb(32, 32, 32)";
+                        // document.querySelector(".darkMode p").style.color = "white";
+                     }
+                  });
+
+
+
             });
       });
    }
 
-   function setIcons(icon, iconID, color){
-      const skycons = new Skycons({color:color});
+   function setIcons(icon, iconID, color) {
+      const skycons = new Skycons({ color: color });
       const currentIcon = icon.replace(/-/g, "_").toUpperCase();
       skycons.play();
       return skycons.set(iconID, Skycons[currentIcon]);
    }
 
+   function replaceSunPositionText(sunPosition) {
+      return currentText = sunPosition.replace(/:/g, "").replace(/ /, "").replace("PM", "").replace("AM", "");
+
+   }
 
 
 });
